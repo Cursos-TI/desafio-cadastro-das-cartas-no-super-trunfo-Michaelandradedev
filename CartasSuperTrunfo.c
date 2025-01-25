e06160f84954f7ae3c736cde55ccc1146df47e57
Michaelandradedev <michaelandradeads@gmail.com>
2025-01-25 00:14:43 +0000

    INCREMENTAÇÃO CODIGO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,30 +1,14 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-// Sugestão: Defina variáveis separadas para cada atributo da cidade.
-// Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-// Cadastro das Cartas:
-// Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-// Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-// Exibição dos Dados das Cartas:
-// Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-// Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
 int main() {
 
 // Sintaxe padrão printf
-//printf("texto com formatação:"", varivavel 1, variavel 2 ...);
+//printf("texto com formatação:", varivavel 1, variavel 2 ...);
 
 // Definição das variáveis:
 
-    char COD[3];
+    char codigoestado;
+    char codigocidade[3];
     char cidade[60];
     float populacao;
     float area;
@@ -33,11 +17,14 @@ int main() {
 
 // Cadastro das informaçoes, essa função solicita ao usuário para inserir as informações que devem constar nas cartas.
 
-printf("Digite o COD da carta: \n");
-scanf("%s", &COD);
+printf("Digite o COD do estado (letra): \n");
+scanf(" %C", &codigoestado);
+
+printf("Digite o COD da cidade (número) \n");
+scanf("  %S", &codigocidade);
 
 printf("Digite o nome da cidade: \n");
-scanf("%s", &cidade);
+scanf("  %s", &cidade);
 
 printf("Digite o número de habitantes: \n");
 scanf(" %f", &populacao);
@@ -53,7 +40,8 @@ scanf("%d", &numerodepontosturisticos);
 
 // Impressão dos valores ordenados de acordo com a organização solicitada.
 
-printf("COD: %d\n Cidade: %s\n População: %f\n Área: %f\n PIB: %f\n Número de pontos turísticos: %d\n", COD, cidade, populacao, area, PIB, numerodepontosturisticos);
+printf("COD: %C0%s\n", codigoestado, codigocidade);
+printf("Cidade: %s\n População: %f\n Área: %f\n PIB: %f\n Número de pontos turísticos: %d\n", cidade, populacao, area, PIB, numerodepontosturisticos);
 
 return 0;
 
